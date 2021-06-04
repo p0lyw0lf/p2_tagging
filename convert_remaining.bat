af9c4d9b3b904d098bb1cd60e32990cd6d0c11df
p0lyw0lf <p0lyw0lf@protonmail.com>
2021-06-03 21:11:21 -0400

    Copy album art w/ metadata in ffmpeg script

--- convert_remaining.bat
+++ convert_remaining.bat
@@ -1,6 +1,6 @@
 for %%f in (*.mp3) do (
-  ffmpeg -y -i "%%f" -vn -c:a alac -q:a 100 -b:a 320000 -map_metadata 0 "%%~nf".m4a
+  ffmpeg -y -i "%%f" -c:a alac -q:a 100 -b:a 320000 -c:v copy -map_metadata 0 "%%~nf".m4a
 )
 for %%f in (*.wav) do (
-  ffmpeg -y -i "%%f" -vn -c:a alac -q:a 100 -map_metadata 0 "%%~nf".m4a
+  ffmpeg -y -i "%%f" -c:a alac -q:a 100 -b:a 320000 -c:v copy -map_metadata 0 "%%~nf".m4a
 )
